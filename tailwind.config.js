c58cf88b383fe5190b5110b5aab659d49694a996
saarthak-pillow <saarthak@pillow.fund>
2023-11-23 22:37:58 -0500

    upload feature implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import { withUt } from "uploadthing/tw";
+ 
+module.exports = withUt({
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -74,3 +76,4 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 }
+);
